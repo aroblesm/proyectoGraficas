ffe5d36b31206f69ba9af4f310491777250db38f
Alejandra Robles <alejandra.robles@ellucian.com>
2021-11-06 19:12:57 -0600

    se agregaron las ventanas

--- galeria/musicGallery.js
+++ galeria/musicGallery.js
@@ -1,13 +1,69 @@
-import * as THREE from '../libs/three.js/r131/three.module.js';
-import { OrbitControls } from '../libs/three.js/r131/controls/OrbitControls.js';
+import * as THREE from '../libs/three.js/r131/three.module.js'
+import { OrbitControls } from '../libs/three.js/r131/controls/OrbitControls.js'
+import { GLTFLoader } from '../libs/three.js/r131/loaders/GLTFLoader.js';
+import { DRACOLoader } from '../libs/three.js/r125/loaders/DRACOLoader.js';
 
 let renderer = null, scene = null, camera = null, orbitControls = null;
-
 let currentTime = Date.now();
 let spotLight = null;
 let ambientLight = null;
 
 const SHADOW_MAP_WIDTH = 2048, SHADOW_MAP_HEIGHT = 2048;
+
+async function loadGLTF() {
+    try {
+        //#region ventanas y tragaluz
+        const dracoLoad = new DRACOLoader();
+        dracoLoad.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.4.0/");
+
+        const gltfLoader = new GLTFLoader();
+        gltfLoader.setDRACOLoader(dracoLoad);
+        const WindowNoGlassL = await gltfLoader.loadAsync("assets/3D/WindowNoGlassL/scene.gltf");
+        const WindowNoGlassR = await gltfLoader.loadAsync("assets/3D/WindowNoGlassR/scene.gltf");
+        const WindowTopNoGlass = await gltfLoader.loadAsync("assets/3D/TopNoGlass/scene.gltf");
+        let newMaterial = new THREE.MeshPhysicalMaterial();
+
+        let windowLeft = WindowNoGlassL.scene.children[0];
+        windowLeft.scale.set(0.04, 0.04, 0.04);
+        windowLeft.position.x = -198;
+        windowLeft.position.z = 100;
+        windowLeft.rotation.z = -Math.PI/2;
+        windowLeft.traverse(child => {
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.material = newMaterial;;
+                child.receiveShadow = true;
+                child.material.metalness = 0.9;
+                child.material.clearcoat = 1;
+                child.material.clearcoatRoughness = 0.6;
+                child.material.roughness = 0.9;
+            }
+        });
+
+        let windowRight = WindowNoGlassR.scene.children[0];
+        windowRight.scale.set(0.04, 0.04, 0.04);
+        windowRight.position.x = -198;
+        windowRight.position.z = -35;
+        windowRight.rotation.z = -Math.PI / 2;
+        windowRight.traverse(child => {
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.material = newMaterial;;
+                child.receiveShadow = true;
+                child.material.metalness = 0.9;
+                child.material.clearcoat = 1;
+                child.material.clearcoatRoughness = 0.6;
+                child.material.roughness = 0.9;
+            }
+        });
+        //#endregion ventanas
+
+        scene.add(windowLeft, windowRight);
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
 
 function animate() {
     const now = Date.now();
@@ -33,7 +89,7 @@ function createScene(canvas) {
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 1, 4000);
+    camera = new THREE.PerspectiveCamera(60, canvas.width / canvas.height, 1, 4000);
     camera.position.set(30, 10, 20);
 
     orbitControls = new OrbitControls(camera, renderer.domElement);
@@ -45,13 +101,15 @@ function createScene(canvas) {
     spotLight.castShadow = true;
     spotLight.shadow.camera.near = 1;
     spotLight.shadow.camera.far = 200;
-    spotLight.shadow.camera.fov = 45;
+    spotLight.shadow.camera.fov = 60;
     spotLight.shadow.mapSize.width = SHADOW_MAP_WIDTH;
     spotLight.shadow.mapSize.height = SHADOW_MAP_HEIGHT;
     scene.add(spotLight);
 
     ambientLight = new THREE.AmbientLight(0xbbbbbb, 1.0);
     scene.add(ambientLight);
+
+    loadGLTF();
 
     //#region Piso de galeria 
     let marbleAlphaMap, marbleMap, marbleNormalMap, grassMap;
@@ -80,7 +138,7 @@ function createScene(canvas) {
     const geometry = new THREE.PlaneGeometry(400, 400);
     const floor = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({ color: color, map: marbleMap, side: THREE.DoubleSide }));
     floor.rotation.x = -Math.PI / 2;
-    floor.position.y = -25;
+    floor.position.y = -75;
     floor.position.z = 22;
     floor.castShadow = false;
     floor.receiveShadow = true;
@@ -93,26 +151,26 @@ function createScene(canvas) {
     texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set(20, 20);
 
-    const geometryWallFront = new THREE.BoxGeometry(400, 50, 1);
+    const geometryWallFront = new THREE.BoxGeometry(400, 150, 1);
     const wallFront = new THREE.Mesh(geometryWallFront, new THREE.MeshPhongMaterial({ color: color, map: marbleAlphaMap, side: THREE.DoubleSide }));
     wallFront.position.z = -178;
     wallFront.castShadow = false;
     wallFront.receiveShadow = true;
 
-    const geometryWallBack = new THREE.BoxGeometry(400, 50, 1);
+    const geometryWallBack = new THREE.BoxGeometry(400, 150, 1);
     const wallBack = new THREE.Mesh(geometryWallBack, new THREE.MeshPhongMaterial({ color: color, map: marbleAlphaMap, side: THREE.DoubleSide }));
     wallBack.position.z = 220;
     wallBack.castShadow = false;
     wallBack.receiveShadow = true;
 
-    const geometryWallLeft = new THREE.BoxGeometry(1, 50, 399);
+    const geometryWallLeft = new THREE.BoxGeometry(1, 150, 399);
     const wallLeft = new THREE.Mesh(geometryWallLeft, new THREE.MeshPhongMaterial({ color: color, map: marbleAlphaMap, side: THREE.DoubleSide }));
     wallLeft.position.x = -200;
     wallLeft.position.z = 20;
     wallLeft.castShadow = false;
     wallLeft.receiveShadow = true;
 
-    const geometryWallRight = new THREE.BoxGeometry(1, 50, 399);
+    const geometryWallRight = new THREE.BoxGeometry(1, 150, 399);
     const wallRight = new THREE.Mesh(geometryWallRight, new THREE.MeshPhongMaterial({ color: color, map: marbleAlphaMap, side: THREE.DoubleSide }));
     wallRight.position.x = 200;
     wallRight.position.z = 20.5;
@@ -121,7 +179,7 @@ function createScene(canvas) {
 
     const geometryWallTop = new THREE.BoxGeometry(400, 1, 400);
     const wallTop = new THREE.Mesh(geometryWallTop, new THREE.MeshPhongMaterial({ color: color, map: marbleAlphaMap, side: THREE.DoubleSide }));
-    wallTop.position.y = 25;
+    wallTop.position.y = 75;
     wallTop.position.z = 20.5;
     wallTop.castShadow = false;
     wallTop.receiveShadow = true;
