acca8006618d1793333a95ebdb8dc2873d0f7583
AhumadaManuel <88684682+AhumadaManuel@users.noreply.github.com>
2021-11-18 17:37:49 -0600

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -35,8 +35,8 @@ if (accessToken) {
         ) +
         `<img id="albumglobal" src="${res.items[0].track.album.images[0].url}"></img>`;
       document.getElementById('albumglobal').addEventListener('click', () => {
-        audioPlayer.playPause(res.items[0].track.preview_url); +
-        `<img id="albumglobal" src="${res.items[0].track.album.images[0].url}"></img>`;
+        audioPlayer.playPause(res.items[0].track.preview_url) +
+        `<img id="albumglobal" src="${res.items[1].track.album.images[1].url}"></img>`;
       document.getElementById('albumglobal').addEventListener('click', () => {
         audioPlayer.playPause(res.items[1].track.preview_url);
       });
